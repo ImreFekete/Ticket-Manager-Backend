f2b3088362ab92fe7647cea475d3c5e77b660e73
Fekete Imre <feketeimi12@gmail.com>
2024-05-27 12:46:17 +0200

    extend application main class

---
+++ src/main/java/com/imrefekete/ticket_manager/TicketManager.java
@@ -0,0 +1,12 @@
+package com.imrefekete.ticket_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketManager.class, args);
+    }
+}
--- src/main/java/com/imrefekete/ticket_manager/Ticket_Manager.java
+++
@@ -1,4 +0,0 @@
-package com.imrefekete.ticket_manager;
-
-public class Ticket_Manager {
-}

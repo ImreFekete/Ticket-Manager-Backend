8fe535c1b4c0f4f6bd6a49070c8c08d3dad96821
Fekete Imre <feketeimi12@gmail.com>
2024-05-28 09:12:32 +0200

    add Screening entity class Added Screening class with JPA annotations, Includes fields: id, movie, auditorium, start, end. Annoted with Lombok for boilerplate code reduction

--- src/main/java/com/imrefekete/ticket_manager/model/Screening.java
+++ src/main/java/com/imrefekete/ticket_manager/model/Screening.java
@@ -1,2 +1,37 @@
-package com.imrefekete.ticket_manager.model;public class s {
+package com.imrefekete.ticket_manager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "screenings")
+public class Screening {
+    @Id
+    @Column(name = "screening_id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "auditorium_id")
+    private Auditorium auditorium;
+
+    @Column(name = "screening_start")
+    private LocalDateTime start;
+
+    @Column(name = "screening_end")
+    private LocalDateTime end;
 }
+
+

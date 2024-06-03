ce6bdb88582078bba02830a79855d9d2776abaad
Fekete Imre <feketeimi12@gmail.com>
2024-06-03 09:59:47 +0200

    add UserRepository interface for the entity:User Implement UserRepository interface extending JpaRepository, add method to find users by username

--- src/main/java/com/imrefekete/ticket_manager/repository/UserRepository.java
+++ src/main/java/com/imrefekete/ticket_manager/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.imrefekete.ticket_manager.repository;
 
-public class UserRepository {
+import com.imrefekete.ticket_manager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findUserByUsername(String username);
 }

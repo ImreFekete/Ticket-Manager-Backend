929b24938560e1267dfcbfd561b38fa8f7b5639a
Fekete Imre <feketeimi12@gmail.com>
2024-06-11 13:12:26 +0200

    Create AuthController for authentication - Added AuthController to handle authentication - Includes a POST endpoint for user login (/api/auth/login), returns a jwt token

--- src/main/java/com/imrefekete/ticket_manager/controller/AuthController.java
+++ src/main/java/com/imrefekete/ticket_manager/controller/AuthController.java
@@ -1,2 +1,22 @@
-package com.imrefekete.ticket_manager.controller;public class AuthController {
+package com.imrefekete.ticket_manager.controller;
+
+import com.imrefekete.ticket_manager.request.AuthRequest;
+import com.imrefekete.ticket_manager.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthenticationService authService;
+
+    public AuthController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        return authService.authenticate(authRequest);
+    }
 }

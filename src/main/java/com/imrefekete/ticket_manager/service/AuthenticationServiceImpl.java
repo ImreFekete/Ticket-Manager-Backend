1295763c97391779ead22d8a30a7054fd7410b3e
Fekete Imre <feketeimi12@gmail.com>
2024-06-11 13:44:51 +0200

    create class AuthenticationServiceImpl it implements AuthenticationService interface authenticate checks user credentials, if valid returns a ResponsEntity with AuthResponse

--- src/main/java/com/imrefekete/ticket_manager/service/AuthenticationServiceImpl.java
+++ src/main/java/com/imrefekete/ticket_manager/service/AuthenticationServiceImpl.java
@@ -1,2 +1,47 @@
-package com.imrefekete.ticket_manager.service;public class AuthenticationServiceImpl {
+package com.imrefekete.ticket_manager.service;
+
+import com.imrefekete.ticket_manager.request.AuthRequest;
+import com.imrefekete.ticket_manager.response.AuthResponse;
+import com.imrefekete.ticket_manager.response.ErrorResponse;
+import com.imrefekete.ticket_manager.security.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+
+    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public ResponseEntity<?> authenticate(AuthRequest authRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()
+                    ));
+            String name = authentication.getName();
+            UserDetails userDetails = userDetailsService.loadUserByUsername(name);
+            String token = jwtService.generateToken(userDetails);
+            AuthResponse loginRes = new AuthResponse(name, token);
+
+            return ResponseEntity.ok(loginRes);
+        } catch (BadCredentialsException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, "Invalid username or password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
 }
+

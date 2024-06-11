53f5e97426d76367808e4641885e441b8396a40a
Fekete Imre <feketeimi12@gmail.com>
2024-06-11 12:03:10 +0200

    Add ErrorResponse DTO for authentication endpoints

--- src/main/java/com/imrefekete/ticket_manager/response/ErrorResponse.java
+++ src/main/java/com/imrefekete/ticket_manager/response/ErrorResponse.java
@@ -1,2 +1,16 @@
-package com.imrefekete.ticket_manager.response;public class ErrorResponse {
+package com.imrefekete.ticket_manager.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private HttpStatus status;
+    private String message;
 }

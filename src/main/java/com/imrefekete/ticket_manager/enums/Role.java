79dc07e8524601b67de71db35585b2d2b980e130
Fekete Imre <feketeimi12@gmail.com>
2024-06-11 13:12:41 +0200

    update Role enum

--- src/main/java/com/imrefekete/ticket_manager/enums/Role.java
+++ src/main/java/com/imrefekete/ticket_manager/enums/Role.java
@@ -1,15 +1,7 @@
 package com.imrefekete.ticket_manager.enums;
 
-import lombok.Getter;
 
-@Getter
 public enum Role {
-    ADMIN("admin"),
-    USER("user");
-
-    private final String role;
-
-    Role(String role) {
-        this.role = role;
-    }
+    ADMIN,
+    USER;
 }

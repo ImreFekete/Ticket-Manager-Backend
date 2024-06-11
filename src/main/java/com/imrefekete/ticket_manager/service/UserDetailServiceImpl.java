972f6c0badff254ad7b238c7c6197264ac74d0bc
Fekete Imre <feketeimi12@gmail.com>
2024-06-11 13:49:17 +0200

    Implement UserDetailServiceImpl for loading user details it implements UserDetailsService interface to load user details from the database based on the username. - Added loadUserByUsername method to retrieve user details by username - Used UserRepository to find the user by username - Threw UsernameNotFoundException if the user is not found - Created UserDetails object with username, password, and role

--- src/main/java/com/imrefekete/ticket_manager/service/UserDetailServiceImpl.java
+++ src/main/java/com/imrefekete/ticket_manager/service/UserDetailServiceImpl.java
@@ -1,2 +1,39 @@
-package com.imrefekete.ticket_manager.service;public class UserDetailServiceImpl {
+package com.imrefekete.ticket_manager.service;
+
+import com.imrefekete.ticket_manager.model.User;
+import com.imrefekete.ticket_manager.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+import static java.lang.String.format;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(
+                                format("User: %s, not found", username)
+                        )
+                );
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole())))
+                .build();
+    }
 }

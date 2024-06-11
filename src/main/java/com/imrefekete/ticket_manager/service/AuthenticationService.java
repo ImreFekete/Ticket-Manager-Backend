dac046e6f956e802fb541d31dd5f89506018130b
Fekete Imre <feketeimi12@gmail.com>
2024-06-11 13:41:58 +0200

    create AuthenticationService interface contains method authenticate

--- src/main/java/com/imrefekete/ticket_manager/service/AuthenticationService.java
+++ src/main/java/com/imrefekete/ticket_manager/service/AuthenticationService.java
@@ -1,2 +1,10 @@
-package com.imrefekete.ticket_manager.service;public interface AuthenticationService {
+package com.imrefekete.ticket_manager.service;
+
+import com.imrefekete.ticket_manager.request.AuthRequest;
+import org.springframework.http.ResponseEntity;
+
+public interface AuthenticationService {
+
+    ResponseEntity<?> authenticate(AuthRequest authRequest);
+
 }

223ebcb6bf0d988435a7869a05f515ef17955cb2
Fekete Imre <feketeimi12@gmail.com>
2024-06-12 15:30:26 +0200

    extend RegisterRequest class

--- src/main/java/com/imrefekete/ticket_manager/model/request/RegisterRequest.java
+++ src/main/java/com/imrefekete/ticket_manager/model/request/RegisterRequest.java
@@ -1,4 +1,18 @@
 package com.imrefekete.ticket_manager.model.request;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String email;
+    private String password;
 }

f7410f2d6666acf0f589b3ead846337f6b07f803
Fekete Imre <feketeimi12@gmail.com>
2024-06-13 11:24:47 +0200

    implement register method from AuthenticationService interface

--- src/main/java/com/imrefekete/ticket_manager/service/AuthenticationServiceImpl.java
+++ src/main/java/com/imrefekete/ticket_manager/service/AuthenticationServiceImpl.java
@@ -1,9 +1,14 @@
 package com.imrefekete.ticket_manager.service;
 
+import com.imrefekete.ticket_manager.enums.Role;
+import com.imrefekete.ticket_manager.model.entity.User;
 import com.imrefekete.ticket_manager.model.request.AuthRequest;
+import com.imrefekete.ticket_manager.model.request.RegisterRequest;
 import com.imrefekete.ticket_manager.model.response.AuthResponse;
 import com.imrefekete.ticket_manager.model.response.ErrorResponse;
+import com.imrefekete.ticket_manager.repository.UserRepository;
 import com.imrefekete.ticket_manager.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,18 +17,40 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtService jwtService) {
+    @Autowired
+    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtService jwtService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
         this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public long register(RegisterRequest registerRequest) {
+        User newUser = User.builder()
+                .username(registerRequest.getUserName())
+                .firstName(registerRequest.getFirstName())
+                .lastName(registerRequest.getLastName())
+                .dateOfBirth(registerRequest.getDateOfBirth())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .email(registerRequest.getEmail())
+                .role(Role.USER)
+                .build();
+
+        return userRepository.save(newUser).getUserId();
     }
 
     @Override

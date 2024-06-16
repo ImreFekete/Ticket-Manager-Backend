4e51b6e503a3617a96b08219706d1ae723448e5f
Fekete Imre <feketeimi12@gmail.com>
2024-06-16 11:11:35 +0200

    add @Getter annotation

--- src/main/java/com/imrefekete/ticket_manager/model/response/AuthResponse.java
+++ src/main/java/com/imrefekete/ticket_manager/model/response/AuthResponse.java
@@ -1,11 +1,9 @@
 package com.imrefekete.ticket_manager.model.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor

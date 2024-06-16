155afac336729d3b6b39746e1dc9caf971ffb9b2
Fekete Imre <feketeimi12@gmail.com>
2024-06-16 10:28:23 +0200

    add method to handle UserNameNotFoundException

--- src/main/java/com/imrefekete/ticket_manager/exception/CustomExceptionHandler.java
+++ src/main/java/com/imrefekete/ticket_manager/exception/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.imrefekete.ticket_manager.exception;
 import com.imrefekete.ticket_manager.model.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +13,12 @@ public class CustomExceptionHandler {
     @ExceptionHandler(DataConflictException.class)
     public ResponseEntity<ErrorResponse> userNameInUseException(DataConflictException e) {
         HttpStatus statusCode = HttpStatus.CONFLICT;
+        ErrorResponse errorResponse = new ErrorResponse(statusCode, e.getMessage());
+        return new ResponseEntity<>(errorResponse, statusCode);
+    }
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> userNotFoundException(UsernameNotFoundException e) {
+        HttpStatus statusCode = HttpStatus.NOT_FOUND;
         ErrorResponse errorResponse = new ErrorResponse(statusCode, e.getMessage());
         return new ResponseEntity<>(errorResponse, statusCode);
     }
